63babe3ac0d9264b9198e0fdd1461b3356bcf2e9
pshanawazkhan <9490549755gmail>
2019-08-15 13:40:34 +0530

    urlconstant shan2

--- src/app/Class/Urls.ts
+++ src/app/Class/Urls.ts
@@ -3,7 +3,13 @@
 export const Urls = {
     
     
-  'splinkersget': 'http://localhost:8080/splinkers/get'
+  'splinkersget': 'http://localhost:8080/splinkers/get',
     
-  
+  'splinkersget1': 'http://localhost:8080/splinkers/get',
+  'splinkersget2': 'http://localhost:8080/splinkers/get',
+  'splinkersget3': 'http://localhost:8080/splinkers/get',
+  'splinkersget4': 'http://localhost:8080/splinkers/get',
+  'splinkersget5': 'http://localhost:8080/splinkers/get',
+  'splinkersget6': 'http://localhost:8080/splinkers/get',
+  'splinkersget7': 'http://localhost:8080/splinkers/get',
 }
