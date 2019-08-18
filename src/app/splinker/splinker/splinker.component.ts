615f1bcc0ac2ca0148a1fcdc4fcc2d16225acb57
pshanawazkhan <9490549755gmail>
2019-08-18 16:55:44 +0530

    2nd

--- src/app/splinker/splinker/splinker.component.ts
+++ src/app/splinker/splinker/splinker.component.ts
@@ -24,6 +24,7 @@ export class SplinkerComponent implements OnInit {
    splServices.getBooksDetailsDetails().subscribe((data)=>{this.splData=data;
   
    console.log("length --------------"+this.splData.length)
+   console.log('log added for splinker file ----------');
   });
 
   }
